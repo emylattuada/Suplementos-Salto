fcf50f180eed2e24a27eb064cf21eb66fc69ed06
LuchoFreitas <luwolfy17@gmail.com>
2023-08-31 15:22:41 -0300

    cambiar pass cambios, aun sin conex a bd.

--- kenjhi/Cambiar Pass.Designer.cs
+++ kenjhi/Cambiar Pass.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cambiar_Pass));
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelArribaCambiarPass = new System.Windows.Forms.Panel();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.txtCambiarContraseña = new System.Windows.Forms.TextBox();
+            this.picMinimizar = new System.Windows.Forms.PictureBox();
+            this.picCerrar = new System.Windows.Forms.PictureBox();
+            this.txtCambiarPassUser = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -43,24 +43,29 @@
             this.txtCambiarContra2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.btnGuardarNuevaPass = new kenjhi.RJButton();
-            this.panel2.SuspendLayout();
+            this.panelArribaCambiarPass.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCerrar)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel2
+            // panelArribaCambiarPass
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.panel2.Controls.Add(this.pictureBox4);
-            this.panel2.Controls.Add(this.pictureBox3);
-            this.panel2.Controls.Add(this.pictureBox2);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(291, 25);
-            this.panel2.TabIndex = 3;
+            this.panelArribaCambiarPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panelArribaCambiarPass.Controls.Add(this.pictureBox4);
+            this.panelArribaCambiarPass.Controls.Add(this.picMinimizar);
+            this.panelArribaCambiarPass.Controls.Add(this.picCerrar);
+            this.panelArribaCambiarPass.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelArribaCambiarPass.Location = new System.Drawing.Point(0, 0);
+            this.panelArribaCambiarPass.Name = "panelArribaCambiarPass";
+            this.panelArribaCambiarPass.Size = new System.Drawing.Size(341, 25);
+            this.panelArribaCambiarPass.TabIndex = 3;
+            this.panelArribaCambiarPass.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelArribaCambiarPass_MouseMove);
             // 
             // pictureBox4
             // 
@@ -73,37 +78,40 @@
             this.pictureBox4.TabStop = false;
             this.pictureBox4.Visible = false;
             // 
-            // pictureBox3
+            // picMinimizar
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(238, 3);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(16, 20);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 1;
-            this.pictureBox3.TabStop = false;
+            this.picMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("picMinimizar.Image")));
+            this.picMinimizar.Location = new System.Drawing.Point(288, 3);
+            this.picMinimizar.Name = "picMinimizar";
+            this.picMinimizar.Size = new System.Drawing.Size(16, 20);
+            this.picMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picMinimizar.TabIndex = 1;
+            this.picMinimizar.TabStop = false;
+            this.picMinimizar.Click += new System.EventHandler(this.picMinimizar_Click);
             // 
-            // pictureBox2
+            // picCerrar
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(260, 1);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(28, 23);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 0;
-            this.pictureBox2.TabStop = false;
+            this.picCerrar.Image = ((System.Drawing.Image)(resources.GetObject("picCerrar.Image")));
+            this.picCerrar.Location = new System.Drawing.Point(310, 1);
+            this.picCerrar.Name = "picCerrar";
+            this.picCerrar.Size = new System.Drawing.Size(28, 23);
+            this.picCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picCerrar.TabIndex = 0;
+            this.picCerrar.TabStop = false;
+            this.picCerrar.Click += new System.EventHandler(this.picCerrar_Click);
             // 
-            // txtCambiarContraseña
+            // txtCambiarPassUser
             // 
-            this.txtCambiarContraseña.BackColor = System.Drawing.Color.Black;
-            this.txtCambiarContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtCambiarContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCambiarContraseña.ForeColor = System.Drawing.Color.White;
-            this.txtCambiarContraseña.Location = new System.Drawing.Point(26, 122);
-            this.txtCambiarContraseña.Multiline = true;
-            this.txtCambiarContraseña.Name = "txtCambiarContraseña";
-            this.txtCambiarContraseña.Size = new System.Drawing.Size(239, 20);
-            this.txtCambiarContraseña.TabIndex = 10;
+            this.txtCambiarPassUser.BackColor = System.Drawing.Color.Black;
+            this.txtCambiarPassUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCambiarPassUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCambiarPassUser.ForeColor = System.Drawing.Color.White;
+            this.txtCambiarPassUser.Location = new System.Drawing.Point(26, 122);
+            this.txtCambiarPassUser.Multiline = true;
+            this.txtCambiarPassUser.Name = "txtCambiarPassUser";
+            this.txtCambiarPassUser.Size = new System.Drawing.Size(288, 20);
+            this.txtCambiarPassUser.TabIndex = 10;
+            this.txtCambiarPassUser.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtCambiarPassUser_MouseClick);
             // 
             // label1
             // 
@@ -125,20 +133,21 @@
             this.textBox1.Location = new System.Drawing.Point(26, 147);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(239, 1);
+            this.textBox1.Size = new System.Drawing.Size(288, 1);
             this.textBox1.TabIndex = 8;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.label2.Location = new System.Drawing.Point(69, 49);
+            this.label2.Location = new System.Drawing.Point(73, 42);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(151, 16);
+            this.label2.Size = new System.Drawing.Size(205, 25);
             this.label2.TabIndex = 11;
-            this.label2.Text = "Restablecer contraseña";
+            this.label2.Text = "Cambiar contraseña";
             // 
             // txtCambiarContra1
             // 
@@ -149,7 +158,7 @@
             this.txtCambiarContra1.Location = new System.Drawing.Point(26, 185);
             this.txtCambiarContra1.Multiline = true;
             this.txtCambiarContra1.Name = "txtCambiarContra1";
-            this.txtCambiarContra1.Size = new System.Drawing.Size(239, 20);
+            this.txtCambiarContra1.Size = new System.Drawing.Size(288, 20);
             this.txtCambiarContra1.TabIndex = 14;
             // 
             // label3
@@ -172,7 +181,7 @@
             this.textBox3.Location = new System.Drawing.Point(26, 210);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(239, 1);
+            this.textBox3.Size = new System.Drawing.Size(288, 1);
             this.textBox3.TabIndex = 12;
             // 
             // txtCambiarContra2
@@ -184,7 +193,7 @@
             this.txtCambiarContra2.Location = new System.Drawing.Point(26, 252);
             this.txtCambiarContra2.Multiline = true;
             this.txtCambiarContra2.Name = "txtCambiarContra2";
-            this.txtCambiarContra2.Size = new System.Drawing.Size(239, 20);
+            this.txtCambiarContra2.Size = new System.Drawing.Size(288, 20);
             this.txtCambiarContra2.TabIndex = 17;
             // 
             // label4
@@ -207,8 +216,56 @@
             this.textBox5.Location = new System.Drawing.Point(26, 277);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(239, 1);
+            this.textBox5.Size = new System.Drawing.Size(288, 1);
             this.textBox5.TabIndex = 15;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(94)))), ((int)(((byte)(95)))));
+            this.lblError.Location = new System.Drawing.Point(54, 300);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(224, 26);
+            this.lblError.TabIndex = 19;
+            this.lblError.Text = "Nombre de usuario o contraseñas incorrectas.\r\n\r\n";
+            this.lblError.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DimGray;
+            this.label5.Location = new System.Drawing.Point(241, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 16);
+            this.label5.TabIndex = 61;
+            this.label5.Text = "Obligatorio";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DimGray;
+            this.label6.Location = new System.Drawing.Point(241, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 16);
+            this.label6.TabIndex = 62;
+            this.label6.Text = "Obligatorio";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DimGray;
+            this.label7.Location = new System.Drawing.Point(241, 230);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 16);
+            this.label7.TabIndex = 63;
+            this.label7.Text = "Obligatorio";
             // 
             // btnGuardarNuevaPass
             // 
@@ -223,7 +280,7 @@
             this.btnGuardarNuevaPass.ForeColor = System.Drawing.Color.Black;
             this.btnGuardarNuevaPass.Location = new System.Drawing.Point(26, 329);
             this.btnGuardarNuevaPass.Name = "btnGuardarNuevaPass";
-            this.btnGuardarNuevaPass.Size = new System.Drawing.Size(239, 44);
+            this.btnGuardarNuevaPass.Size = new System.Drawing.Size(288, 44);
             this.btnGuardarNuevaPass.TabIndex = 18;
             this.btnGuardarNuevaPass.Text = "Guardar";
             this.btnGuardarNuevaPass.TextColor = System.Drawing.Color.Black;
@@ -234,7 +291,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(291, 419);
+            this.ClientSize = new System.Drawing.Size(341, 419);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblError);
             this.Controls.Add(this.btnGuardarNuevaPass);
             this.Controls.Add(this.txtCambiarContra2);
             this.Controls.Add(this.label4);
@@ -243,18 +304,19 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtCambiarContraseña);
+            this.Controls.Add(this.txtCambiarPassUser);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelArribaCambiarPass);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Cambiar_Pass";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Cambiar_Pass";
-            this.panel2.ResumeLayout(false);
+            this.Text = "Suplementos Salto";
+            this.panelArribaCambiarPass.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCerrar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -262,11 +324,11 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox picMinimizar;
+        private System.Windows.Forms.PictureBox picCerrar;
+        private System.Windows.Forms.Panel panelArribaCambiarPass;
         private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.TextBox txtCambiarContraseña;
+        private System.Windows.Forms.TextBox txtCambiarPassUser;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
@@ -277,5 +339,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox5;
         private RJButton btnGuardarNuevaPass;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
--- kenjhi/Cambiar Pass.cs
+++ kenjhi/Cambiar Pass.cs
@@ -15,6 +15,55 @@ namespace kenjhi
         public Cambiar_Pass()
         {
             InitializeComponent();
+            txtCambiarPassUser.Text = "Ingresa tu usuario";
+            txtCambiarContra1.Text = "Nueva contraseña";
+            txtCambiarContra2.Text = "Nueva contraseña";
+            txtCambiarContra1.PasswordChar = '*';
+            txtCambiarContra2.PasswordChar = '*';
+            txtCambiarPassUser.ForeColor = System.Drawing.Color.DarkGray;
+            txtCambiarContra1.ForeColor = System.Drawing.Color.DarkGray;
+            txtCambiarContra2.ForeColor = System.Drawing.Color.DarkGray;
+
+
+
+
+        }
+        private int ClickX = 0, ClickY = 0;
+
+        private void picCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void picMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+
+        }
+
+        private void panelArribaCambiarPass_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                ClickX = e.X;
+                ClickY = e.Y;
+            }
+            else
+            {
+                this.Left = this.Left + (e.X - ClickX);
+                this.Top = this.Top + (e.Y - ClickY);
+            }
+        }
+
+        private void txtCambiarPassUser_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (txtCambiarPassUser.Text == "Ingresa tu usuario") { txtCambiarPassUser.Clear(); txtCambiarPassUser.ForeColor = System.Drawing.Color.White; }
+
+            if (txtCambiarContra1.Text == "Nueva contraseña") { txtCambiarContra1.Clear(); txtCambiarContra1.ForeColor = System.Drawing.Color.White; }
+
+            if (txtCambiarContra2.Text == "Nueva contraseña") { txtCambiarContra2.Clear(); txtCambiarContra2.ForeColor = System.Drawing.Color.White; }
+
         }
     }
 }
