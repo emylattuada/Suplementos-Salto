3f196352ff371640c67fe18f2be0d0629efd39d1
Emyruuu <5dxuruguay@gmail.com>
2023-09-06 15:48:34 -0300

    envio de email implementado, faltan las demas acciones. enable el txt de codigo, subir code nuevo a bd, verificar code, txt de password, y subida de datos a bd

---
+++ kenjhi/Cambiar_Pass.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace kenjhi
+{
+    partial class Cambiar_Pass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cambiar_Pass));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCambiarUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCambiarEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnVerificarCorreo = new kenjhi.RJButton();
+            this.btnEnviarCodigo = new kenjhi.RJButton();
+            this.txtCambiarCodigo = new System.Windows.Forms.TextBox();
+            this.labelCodigo = new System.Windows.Forms.Label();
+            this.txtCodigoAbajo = new System.Windows.Forms.TextBox();
+            this.labelCodigo2 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(291, 25);
+            this.panel2.TabIndex = 3;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(3, 1);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(28, 23);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(238, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(16, 20);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(260, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 23);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(68, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cambiar contraseña";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtCambiarUsuario
+            // 
+            this.txtCambiarUsuario.BackColor = System.Drawing.Color.Black;
+            this.txtCambiarUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCambiarUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCambiarUsuario.ForeColor = System.Drawing.Color.White;
+            this.txtCambiarUsuario.Location = new System.Drawing.Point(26, 116);
+            this.txtCambiarUsuario.Multiline = true;
+            this.txtCambiarUsuario.Name = "txtCambiarUsuario";
+            this.txtCambiarUsuario.Size = new System.Drawing.Size(239, 20);
+            this.txtCambiarUsuario.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(23, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Usuario";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Yellow;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(26, 141);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 1);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DimGray;
+            this.label5.Location = new System.Drawing.Point(192, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 16);
+            this.label5.TabIndex = 57;
+            this.label5.Text = "Obligatorio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(86, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 61;
+            this.label3.Text = "Obligatorio";
+            // 
+            // txtCambiarEmail
+            // 
+            this.txtCambiarEmail.BackColor = System.Drawing.Color.Black;
+            this.txtCambiarEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCambiarEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCambiarEmail.ForeColor = System.Drawing.Color.White;
+            this.txtCambiarEmail.Location = new System.Drawing.Point(26, 175);
+            this.txtCambiarEmail.Multiline = true;
+            this.txtCambiarEmail.Name = "txtCambiarEmail";
+            this.txtCambiarEmail.Size = new System.Drawing.Size(133, 20);
+            this.txtCambiarEmail.TabIndex = 60;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(23, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 59;
+            this.label4.Text = "Email";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Yellow;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(26, 200);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(133, 1);
+            this.textBox3.TabIndex = 58;
+            // 
+            // btnVerificarCorreo
+            // 
+            this.btnVerificarCorreo.BackColor = System.Drawing.Color.Yellow;
+            this.btnVerificarCorreo.BackgroundColor = System.Drawing.Color.Yellow;
+            this.btnVerificarCorreo.BorderColor = System.Drawing.Color.Transparent;
+            this.btnVerificarCorreo.BorderRadius = 10;
+            this.btnVerificarCorreo.BorderSize = 0;
+            this.btnVerificarCorreo.FlatAppearance.BorderSize = 0;
+            this.btnVerificarCorreo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVerificarCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificarCorreo.ForeColor = System.Drawing.Color.Black;
+            this.btnVerificarCorreo.Location = new System.Drawing.Point(166, 168);
+            this.btnVerificarCorreo.Name = "btnVerificarCorreo";
+            this.btnVerificarCorreo.Size = new System.Drawing.Size(99, 30);
+            this.btnVerificarCorreo.TabIndex = 62;
+            this.btnVerificarCorreo.Text = "Verificar";
+            this.btnVerificarCorreo.TextColor = System.Drawing.Color.Black;
+            this.btnVerificarCorreo.UseVisualStyleBackColor = false;
+            this.btnVerificarCorreo.Click += new System.EventHandler(this.btnVerificarCorreo_Click);
+            // 
+            // btnEnviarCodigo
+            // 
+            this.btnEnviarCodigo.BackColor = System.Drawing.Color.Yellow;
+            this.btnEnviarCodigo.BackgroundColor = System.Drawing.Color.Yellow;
+            this.btnEnviarCodigo.BorderColor = System.Drawing.Color.Transparent;
+            this.btnEnviarCodigo.BorderRadius = 10;
+            this.btnEnviarCodigo.BorderSize = 0;
+            this.btnEnviarCodigo.Enabled = false;
+            this.btnEnviarCodigo.FlatAppearance.BorderSize = 0;
+            this.btnEnviarCodigo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnviarCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviarCodigo.ForeColor = System.Drawing.Color.Black;
+            this.btnEnviarCodigo.Location = new System.Drawing.Point(166, 228);
+            this.btnEnviarCodigo.Name = "btnEnviarCodigo";
+            this.btnEnviarCodigo.Size = new System.Drawing.Size(99, 30);
+            this.btnEnviarCodigo.TabIndex = 67;
+            this.btnEnviarCodigo.Text = "Enviar";
+            this.btnEnviarCodigo.TextColor = System.Drawing.Color.Black;
+            this.btnEnviarCodigo.UseVisualStyleBackColor = false;
+            this.btnEnviarCodigo.Click += new System.EventHandler(this.btnEnviarCodigo_Click);
+            // 
+            // txtCambiarCodigo
+            // 
+            this.txtCambiarCodigo.BackColor = System.Drawing.Color.Black;
+            this.txtCambiarCodigo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCambiarCodigo.Enabled = false;
+            this.txtCambiarCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCambiarCodigo.ForeColor = System.Drawing.Color.White;
+            this.txtCambiarCodigo.Location = new System.Drawing.Point(26, 235);
+            this.txtCambiarCodigo.Multiline = true;
+            this.txtCambiarCodigo.Name = "txtCambiarCodigo";
+            this.txtCambiarCodigo.Size = new System.Drawing.Size(133, 20);
+            this.txtCambiarCodigo.TabIndex = 65;
+            this.txtCambiarCodigo.Visible = false;
+            // 
+            // labelCodigo
+            // 
+            this.labelCodigo.AutoSize = true;
+            this.labelCodigo.BackColor = System.Drawing.Color.Transparent;
+            this.labelCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCodigo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.labelCodigo.Location = new System.Drawing.Point(23, 213);
+            this.labelCodigo.Name = "labelCodigo";
+            this.labelCodigo.Size = new System.Drawing.Size(51, 16);
+            this.labelCodigo.TabIndex = 64;
+            this.labelCodigo.Text = "Código";
+            // 
+            // txtCodigoAbajo
+            // 
+            this.txtCodigoAbajo.BackColor = System.Drawing.Color.Yellow;
+            this.txtCodigoAbajo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCodigoAbajo.Enabled = false;
+            this.txtCodigoAbajo.Location = new System.Drawing.Point(26, 260);
+            this.txtCodigoAbajo.Multiline = true;
+            this.txtCodigoAbajo.Name = "txtCodigoAbajo";
+            this.txtCodigoAbajo.Size = new System.Drawing.Size(133, 1);
+            this.txtCodigoAbajo.TabIndex = 63;
+            this.txtCodigoAbajo.Visible = false;
+            // 
+            // labelCodigo2
+            // 
+            this.labelCodigo2.AutoSize = true;
+            this.labelCodigo2.BackColor = System.Drawing.Color.Transparent;
+            this.labelCodigo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCodigo2.ForeColor = System.Drawing.Color.DimGray;
+            this.labelCodigo2.Location = new System.Drawing.Point(86, 213);
+            this.labelCodigo2.Name = "labelCodigo2";
+            this.labelCodigo2.Size = new System.Drawing.Size(73, 16);
+            this.labelCodigo2.TabIndex = 66;
+            this.labelCodigo2.Text = "Obligatorio";
+            // 
+            // Cambiar_Pass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(291, 422);
+            this.Controls.Add(this.btnEnviarCodigo);
+            this.Controls.Add(this.labelCodigo2);
+            this.Controls.Add(this.txtCambiarCodigo);
+            this.Controls.Add(this.labelCodigo);
+            this.Controls.Add(this.txtCodigoAbajo);
+            this.Controls.Add(this.btnVerificarCorreo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCambiarEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCambiarUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Cambiar_Pass";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cambiar_Pass";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCambiarUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCambiarEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private RJButton btnVerificarCorreo;
+        private RJButton btnEnviarCodigo;
+        private System.Windows.Forms.TextBox txtCambiarCodigo;
+        private System.Windows.Forms.Label labelCodigo;
+        private System.Windows.Forms.TextBox txtCodigoAbajo;
+        private System.Windows.Forms.Label labelCodigo2;
+    }
+}
---
+++ kenjhi/Cambiar_Pass.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using MySql.Data.MySqlClient;
+
+namespace kenjhi
+{
+    public partial class Cambiar_Pass : Form
+    {
+        public Cambiar_Pass()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificarCorreo_Click(object sender, EventArgs e)
+        {
+            string usuario = txtCambiarUsuario.Text;
+            string correo = txtCambiarEmail.Text;
+
+            string connectionString = "Server=localhost; Database=suple; Uid=jhin; Pwd=jhin444_2023;";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM usuarios WHERE NombreUsuario = @Usuario AND email = @Email";
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Usuario", usuario);
+                    cmd.Parameters.AddWithValue("@Email", correo);
+
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        MessageBox.Show("Usuario y correo válidos.");
+                        txtCambiarCodigo.Enabled = true;
+                        btnEnviarCodigo.Enabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o correo no válidos.");
+                    }
+                }
+            }
+        }
+
+        private void btnEnviarCodigo_Click(object sender, EventArgs e)
+        {
+            string correoOrigen = "info3emt@edusalto.uy"; // Correo de origen
+            string contraseniaOrigen = "info3emt21"; // Contraseña del correo de origen
+            string correoDestino = txtCambiarEmail.Text; // Correo de destino
+            string codigo = GenerarCodigoVerificacion(); // Genera el code de verificar
+
+            string mensaje = $"Tu código de verificación es: {codigo}";
+
+            try
+            {
+                enviarCorreo(correoOrigen, contraseniaOrigen, correoDestino, mensaje);
+                MessageBox.Show("Mensaje enviado al correo. Ingresa el código de 6 dígitos.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al enviar el correo: {ex.Message}");
+            }
+        }
+
+        private void enviarCorreo(string emisor, string contrasenia, string receptor, string mensaje)
+        {
+            SmtpClient cliente = new SmtpClient();
+            cliente.DeliveryMethod = SmtpDeliveryMethod.Network;
+            cliente.Port = 587; 
+            cliente.EnableSsl = false; 
+            cliente.Credentials = new NetworkCredential(emisor, contrasenia);
+            cliente.Host = "mail.edusalto.uy"; 
+
+            MailMessage correo = new MailMessage();
+
+            correo.To.Add(receptor);
+
+            correo.Subject = "Código de verificación"; // Asunto del correo
+            correo.SubjectEncoding = Encoding.UTF8;
+
+            correo.Body = mensaje;
+            correo.BodyEncoding = Encoding.UTF8;
+            correo.IsBodyHtml = true;
+
+            correo.From = new MailAddress(emisor);
+
+            cliente.Send(correo);
+        }
+
+        private string GenerarCodigoVerificacion()
+        {
+            // generamos un codigo aleatorio para reponerlo en la bd, proximamente agregado
+            Random rand = new Random();
+            int codigo = rand.Next(100000, 999999);
+            return codigo.ToString();
+        }
+    }
+}
