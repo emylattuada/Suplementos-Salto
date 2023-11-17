600cc0b82c564fd4f412b7e0809cc173f2327bce
Emyruuu <emjuan1234@gmail.com>
2023-11-17 18:55:19 -0300

    cambio de consultas y metodos para que cliente contenga CI, tambien agregado en bd. falta buscador y hacer lo mismo en form empleado. Pedido de fitzgerald

--- kenjhi/frmsAdmin/frmAgregarCliente_Admin.cs
+++ kenjhi/frmsAdmin/frmAgregarCliente_Admin.cs
@@ -22,7 +22,7 @@ namespace kenjhi
 
         private void btnAgregarCliente_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(txtNombreCliente.Text) || string.IsNullOrWhiteSpace(txtNumeroCliente.Text))
+            if (string.IsNullOrWhiteSpace(txtNombreCliente.Text) || string.IsNullOrWhiteSpace(txtNumeroCliente.Text) || string.IsNullOrEmpty(txtCICliente.Text))
             {
                 lbl2.Visible = true;
                 System.Media.SystemSounds.Exclamation.Play();
@@ -31,8 +31,9 @@ namespace kenjhi
 
             string nombre = txtNombreCliente.Text;
             string telefono = txtNumeroCliente.Text;
-            string direccion = txtDirCliente.Text;
+            string direccion = txtCICliente.Text;
             string email = txtEmail.Text;
+            string ci = txtCICliente.Text;
 
             if (!string.IsNullOrEmpty(email) && !Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"))
             {
@@ -64,71 +65,74 @@ namespace kenjhi
             }
             else
             {
-                AgregarCliente(nombre, telefono, direccion, email);
+                AgregarCliente(nombre, telefono, direccion, email, ci);
                 MessageBox.Show("Nuevo cliente ingresado correctamente.", "Nuevo cliente", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 LimpiarCampos();
             }
         }
 
-        private bool VerificarExistenciaCliente(string nombre, string telefono)
+        private bool VerificarExistenciaCliente(string nombre, string ci)
         {
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-                string query = "SELECT COUNT(*) FROM Cliente WHERE Nombre = @Nombre AND Telefono = @Telefono";
+                string query = "SELECT COUNT(*) FROM Cliente WHERE Nombre = @Nombre AND CI = @CI";
                 using (MySqlCommand command = new MySqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Nombre", nombre);
-                    command.Parameters.AddWithValue("@Telefono", telefono);
+                    command.Parameters.AddWithValue("@CI", ci);
                     int count = Convert.ToInt32(command.ExecuteScalar());
                     return count > 0;
                 }
             }
         }
 
-        private bool VerificarClienteEliminado(string nombre, string telefono)
+        private bool VerificarClienteEliminado(string nombre, string ci)
         {
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-                string query = "SELECT COUNT(*) FROM Cliente WHERE Nombre = @Nombre AND Telefono = @Telefono AND Visible = 0";
+                string query = "SELECT COUNT(*) FROM Cliente WHERE Nombre = @Nombre AND CI = @CI AND Visible = 0";
                 using (MySqlCommand command = new MySqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Nombre", nombre);
-                    command.Parameters.AddWithValue("@Telefono", telefono);
+                    command.Parameters.AddWithValue("@CI", ci);
                     int count = Convert.ToInt32(command.ExecuteScalar());
                     return count > 0;
                 }
             }
         }
 
-        private void RecuperarCliente(string nombre, string telefono)
+        private void RecuperarCliente(string nombre, string ci)
         {
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-                string query = "UPDATE Cliente SET Visible = 1 WHERE Nombre = @Nombre AND Telefono = @Telefono";
+                string query = "UPDATE Cliente SET Visible = 1 WHERE Nombre = @Nombre AND CI = @CI";
                 using (MySqlCommand command = new MySqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Nombre", nombre);
-                    command.Parameters.AddWithValue("@Telefono", telefono);
+                    command.Parameters.AddWithValue("@CI", ci);
                     command.ExecuteNonQuery();
                 }
             }
         }
 
-        private void AgregarCliente(string nombre, string telefono, string direccion, string email)
+        private void AgregarCliente(string nombre, string telefono, string direccion, string email, string ci)
         {
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-                string query = "INSERT INTO Cliente (Nombre, Telefono, Direccion, Email) VALUES (@Nombre, @Telefono, @Direccion, @Email)";
+                string query = "INSERT INTO Cliente (Nombre, Telefono, Direccion, Email, CI) VALUES (@Nombre, @Telefono, @Direccion, @Email, CI)";
                 using (MySqlCommand command = new MySqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Nombre", nombre);
                     command.Parameters.AddWithValue("@Telefono", telefono);
                     command.Parameters.AddWithValue("@Direccion", direccion);
                     command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@CI", ci);
+
+
                     command.ExecuteNonQuery();
                 }
             }
@@ -138,7 +142,7 @@ namespace kenjhi
         {
             txtNombreCliente.Text = "";
             txtNumeroCliente.Text = "";
-            txtDirCliente.Text = "";
+            txtCICliente.Text = "";
             txtEmail.Text = "";
             lbl2.Visible = false;
         }
